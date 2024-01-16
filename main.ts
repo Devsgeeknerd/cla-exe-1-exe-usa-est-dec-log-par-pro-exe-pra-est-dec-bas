e9e76a4d118ec0a894e76aa0de00742d975d0945
Paulo <vanderley.1109@gmail.com>
2024-01-16 10:54:38 -0300

    adicionando conteudo

---
+++ main.ts
@@ -0,0 +1,9 @@
+let idadePessoa: number = 18;
+
+if (idadePessoa >= 18) {
+  console.log("Você pode entrar na montanha-russa, Divirta-se!");
+} else {
+  console.log(
+    "Desculpe, você precisa ter pelo menos 18 anos para entrar na montanha-russa."
+  );
+}
