6199aa53dc3f7dc7f457147eba1ae967439c9f19
Paulo Vanderley <vanderley.1109@gmail.com>
2024-02-15 15:22:00 -0300

    atualizando conteudo

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
+// Declaração de uma variável 'idadePessoa' do tipo number com valor 18.
 let idadePessoa: number = 18;
 
+// Verifica se a 'idadePessoa' é maior ou igual a 18 anos.
 if (idadePessoa >= 18) {
+  // Se a condição for verdadeira, imprime uma mensagem permitindo a entrada na montanha-russa.
   console.log("Você pode entrar na montanha-russa, Divirta-se!");
 } else {
+  // Se a condição for falsa, imprime uma mensagem informando que é necessário ter pelo menos 18 anos.
   console.log(
     "Desculpe, você precisa ter pelo menos 18 anos para entrar na montanha-russa."
   );
